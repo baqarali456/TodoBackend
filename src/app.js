c608b97f0dbe81b992dc4007a50d78c93844ac37
baqar <sayedalintl@gmail.com>
2024-07-03 12:17:16 +0530

    update todobackend

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({limit:"16kb",extended:true}))
-app.use(express.static("dist"))
+app.use(express.static("public"))
 app.use(cookieParser())
 
 // import routes
